94a5be17860c208d7ea07ae3f6c31151c63a6609
Adam Speakman <adam@speakman.net.nz>
2013-10-28 22:12:58 +1300

    Updated launcher icon, screen name display

--- src/nz/net/speakman/android/dreamintweets/activities/MainActivity.java
+++ src/nz/net/speakman/android/dreamintweets/activities/MainActivity.java
@@ -16,16 +16,21 @@
 
 package nz.net.speakman.android.dreamintweets.activities;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import nz.net.speakman.android.dreamintweets.DreamApplication;
 import nz.net.speakman.android.dreamintweets.R;
 import nz.net.speakman.android.dreamintweets.preferences.DreamPreferences;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Build.VERSION_CODES;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -37,13 +42,14 @@ public class MainActivity extends Activity {
             return;
         }
         setContentView(R.layout.activity_main);
+        showScreenName();
     }
-    
+
     public void onSignOutButtonClick(View v) {
         new DreamPreferences(this).logoutUser();
         startSignInActivity();
     }
-    
+
     @SuppressLint("InlinedApi")
     public void onSettingsButtonClick(View v) {
         Intent intent;
@@ -54,15 +60,48 @@ public class MainActivity extends Activity {
         }
         startActivity(intent);
     }
-    
+
     private void startSignInActivity() {
         Intent i = new Intent(this, SignInActivity.class);
         startActivity(i);
         finish();
     }
-    
+
     private boolean userIsLoggedIn() {
         return new DreamPreferences(this).userIsLoggedIn();
+    }
+
+    private void showScreenName() {
+        new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected String doInBackground(Void... params) {
+                String screenname = null;
+                try {
+                    Twitter twitter = ((DreamApplication)getApplication()).getTwitter();
+                    twitter.setOAuthAccessToken(new DreamPreferences(MainActivity.this).getAccessToken());
+                    screenname = twitter.getScreenName();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (TwitterException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return screenname;
+            }
+            
+            @Override
+            protected void onPostExecute(String result) {
+                TextView tv = (TextView) findViewById(R.id.signed_in_as_text);
+                if (tv != null && result != null) {
+                    tv.setText(getString(R.string.main_activity_signed_in_as,
+                            result));
+                    tv.setVisibility(View.VISIBLE);
+                }
+            }
+
+        }.execute();
     }
 
 }
